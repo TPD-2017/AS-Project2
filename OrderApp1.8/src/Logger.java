feafd0c7728471b40872ce75940a1a866da00006
Pedro <pedro_7915@hotmail.com>
2016-03-17 23:47:30 +0000

    Logger logs Shipments, possibly finalized

--- OrderApp1.8/src/Logger.java
+++ OrderApp1.8/src/Logger.java
@@ -1,15 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.logging.Level;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
