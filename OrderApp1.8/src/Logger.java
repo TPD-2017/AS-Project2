705678b43ee759262da3c3e83cc5e9983df87113
Pedro <pedro_7915@hotmail.com>
2016-03-17 23:00:36 +0000

    Logger logs Shipments, possibly finalized

--- OrderApp1.8/src/Logger.java
+++ OrderApp1.8/src/Logger.java
@@ -21,6 +21,15 @@ public class Logger {
     private File filepathOrder;
     private File filepathShip;
     
+    public Logger(String filepathShip){
+        this.filepathShip = new File(filepathShip);
+    }
+    
+    public Logger(String filepathLogin, String filepathOrder){
+        this.filepathLogin = new File(filepathLogin);
+        this.filepathOrder = new File(filepathOrder);
+    }
+    
     public Logger(String filepathLogin, String filepathOrder, String filepathShip){
         this.filepathLogin = new File(filepathLogin);
         this.filepathOrder = new File(filepathOrder);
@@ -28,27 +37,40 @@ public class Logger {
     }
     
     public void logLogin(String username){
-        boolean res = false;
         try{
             FileWriter fw = new FileWriter(filepathLogin, true);
             BufferedWriter bw = new BufferedWriter(fw);
             bw.append("[Login] User \'"+username+"\' logged in.\n");
             bw.close();
             fw.close();
-        } catch (IOException ex) { 
+        } catch (IOException ex) {
+            System.out.println("Logger error:");
             java.util.logging.Logger.getLogger(Logger.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
     public void logOrder(String username, String orderID, String timestamp){
-        boolean res = false;
         try{
             FileWriter fw = new FileWriter(filepathOrder, true);
             BufferedWriter bw = new BufferedWriter(fw);
             bw.append("[Order "+orderID+"] "+timestamp+": User \'"+username+"\' added order.\n");
             bw.close();
             fw.close();
-        } catch (IOException ex) { 
+        } catch (IOException ex) {
+            System.out.println("Logger error:");
+            java.util.logging.Logger.getLogger(Logger.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void logShip(String orderID, String timestamp){
+        try{
+            FileWriter fw = new FileWriter(filepathShip, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.append("[Shipment] "+timestamp+" Order "+orderID+" was shipped.\n");
+            bw.close();
+            fw.close();
+        } catch (IOException ex) {
+            System.out.println("Logger error:");
             java.util.logging.Logger.getLogger(Logger.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
